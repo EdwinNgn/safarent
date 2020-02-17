9793f4d8d1410516f5dcf62d8ce07e2cf39c85b8
Edwin Nguyen <edwin.nguyen@edhec.com>
2020-02-17 17:09:17 +0100

    erreur

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,20 @@ ActiveRecord::Schema.define(version: 2020_02_17_153338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "animals", force: :cascade do |t|
+    t.string "name"
+    t.string "animal_type"
+    t.string "species"
+    t.integer "price_per_day"
+    t.string "location"
+    t.text "description"
+    t.boolean "display", default: true
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_animals_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_02_17_153338) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "animals", "users"
 end
