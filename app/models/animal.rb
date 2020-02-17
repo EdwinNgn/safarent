46cff13b931ae8763dd0adce0d033d8deeb268d7
Paul Chapeau <paul.chapeau94@gmail.com>
2020-02-17 16:58:00 +0100

    modfication animal model

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,8 +1,9 @@
 class Animal < ApplicationRecord
-  validate :name, presence: true, uniqueness: true
+  validate :name, presence: true
   validate :animal_type, presence: true
   validate :price_per_day, presence: true
   validate :location, presence: true
+  validate :species, presence: true
   validate :description, presence: true
   has_many :bookings
   has_many :reviews, through: :bookings
--- db/migrate/20200217152908_create_animals.rb
+++ db/migrate/20200217152908_create_animals.rb
@@ -3,9 +3,11 @@ class CreateAnimals < ActiveRecord::Migration[5.2]
     create_table :animals do |t|
       t.string :name
       t.string :animal_type
+      t.string :species
       t.integer :price_per_day
       t.string :location
       t.text :description
+      t.boolean :display, default: true
       t.references :user, foreign_key: true
       t.timestamps
     end
