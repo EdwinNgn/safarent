66048300fe20ef4aff3f5e8e5d622cd3bea8f2a7
Paul Chapeau <paul.chapeau94@gmail.com>
2020-02-20 09:26:43 +0100

    en cours

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @animals = Animal.all.map{ |animal| animal.location}.uniq
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,3 +6,10 @@ import { initMapbox } from '../plugins/init_mapbox';
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
 initMapbox();
+
+
+
+import 'select2/dist/css/select2.css';
+import { initSelect2 } from '../components/init_select2';
+
+initSelect2();
