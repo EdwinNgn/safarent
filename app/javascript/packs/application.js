f5276a88c3e85795ed16f1b6d049fb8aa2da10fa
Rose MOulan <rose.moulan@edhec.com>
2020-02-20 17:43:55 +0100

    price adjusts with number of days

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,11 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import "../plugins/flatpickr"
+import "../components/price"
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { loadDynamicBannerText } from '../components/banner';
 initMapbox();
 loadDynamicBannerText();
 initMapbox();
+
