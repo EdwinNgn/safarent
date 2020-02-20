b04f2538e46cd49fc2c633e7eb88a804e7fb2475
Tom Cizeron <tom.cizeron@edhec.com>
2020-02-20 18:25:32 +0100

    Trier les animaux par prix et catégorie, c'est désomrais possible youhou

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,12 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import "../plugins/flatpickr"
+import "../components/price_filter"
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { loadDynamicBannerText } from '../components/banner';
 initMapbox();
 loadDynamicBannerText();
 initMapbox();
+
+
