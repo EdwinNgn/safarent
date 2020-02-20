70ac8a28b42dbeb1bcb8c109a508a923baca75a4
Edwin Nguyen <edwin.nguyen@edhec.com>
2020-02-20 15:49:18 +0100

    first step

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,11 @@ class ApplicationController < ActionController::Base
   end
 
   def booking_notifications
-    notif_owner = current_user.bookings.where(status: "pending", read: false)
-    flash[:notice] = "Hey, #{current_user.first_name}. You have pending approval. Let's have a look on your profil ;)" if notif_owner.any?
-    notif_user = current_user.bookings.where(status: "accept", read: false)
-    flash[:notice] = "Congrats, #{current_user.first_name}. One of your Booking have been Approved. Let's have a look on your profil ;)" if notif_user.any?
+    unless current_user.nil?
+      notif_owner = current_user.bookings.where(status: "pending", read: false)
+      flash[:notice] = "Hey, #{current_user.first_name}. You have pending approval. Let's have a look on your profil ;)" if notif_owner.any?
+      notif_user = current_user.bookings.where(status: "accept", read: false)
+      flash[:notice] = "Congrats, #{current_user.first_name}. One of your Booking have been Approved. Let's have a look on your profil ;)" if notif_user.any?
+    end
   end
 end
