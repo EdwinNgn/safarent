f4bd6a0c1e4c1f1a4046415e7f524224bd507132
Edwin Nguyen <edwin.nguyen@edhec.com>
2020-02-20 20:22:33 +0100

    remove filter js for typed to work

--- app/javascript/components/price_filter.js
+++ app/javascript/components/price_filter.js
@@ -1,5 +1,5 @@
-const divPrice = document.getElementById("price");
-divPrice.addEventListener("change", (event) => {
-  const label = event.target.labels[0]
-  label.innerText = `Less than : ${event.target.value}$`
-});
+// const divPrice = document.getElementById("price");
+// divPrice.addEventListener("change", (event) => {
+//   const label = event.target.labels[0]
+//   label.innerText = `Less than : ${event.target.value}$`
+// });
