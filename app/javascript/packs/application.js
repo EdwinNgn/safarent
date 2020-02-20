b03bc7abcc10b3fd3a400d4a1bb6e86d023a2aba
Rose MOulan <rose.moulan@edhec.com>
2020-02-20 09:50:54 +0100

    conflict

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,5 @@ import "../plugins/flatpickr"
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { loadDynamicBannerText } from '../components/banner';
-
 loadDynamicBannerText();
 initMapbox();
