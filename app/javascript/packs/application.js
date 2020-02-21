252c6c044ee9fd5c5e7e0831fae0ec40d07438c8
Tom Cizeron <tom.cizeron@edhec.com>
2020-02-21 11:24:40 +0100

    sort price ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,12 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import "../plugins/flatpickr"
+import { loadDynamicBannerText } from '../components/banner';
 import "../components/price_filter"
 import "../components/price"
 
 import { initMapbox } from '../plugins/init_mapbox';
-import { loadDynamicBannerText } from '../components/banner';
+
 initMapbox();
 loadDynamicBannerText();
 initMapbox();
